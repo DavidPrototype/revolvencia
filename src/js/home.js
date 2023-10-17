c66268b7e6bb78ce8f2b186e2435b7cd1df79361
David Sanchez <dsanchez@prototype.mx>
2023-10-17 09:40:08 -0600

    fix:validaciones calucladora

--- src/js/home.js
+++ src/js/home.js
@@ -44,40 +44,76 @@ const msgErrorPago = document.querySelector('.error-pago .mensaje');
 
 let saldoMensual =0;
 let pago =0;
+/// NUMEROS
+let regExNumber = /^[0-9]+$/;
+
+
+
 
 document.querySelector('#saldo-pendiente').addEventListener('input',(event)=>{
     alertErrorSaldo.classList.add('d-none')    
-    saldoMensual = parseFloat(event.target.value);
-  
-
-    if( saldoMensual <= 0 ){
+    //saldoMensual = parseFloat(event.target.value);
+    
+    if(event.target.value.length < 1 || isNaN(event.target.value.length)){
         alertErrorSaldo.classList.remove('d-none');
         msgErrorSaldo.innerText ='Debes ingresar un monto para calcular';
         btnCalcular.disabled = true;  
-    }else if(saldoMensual <= 100){
-        alertErrorSaldo.classList.remove('d-none');
-        msgErrorSaldo.innerText ='El monto mínimo debe ser mayor a 100 pesos';
-        btnCalcular.disabled = true;  
     }else{
-        btnCalcular.disabled = false;
+            let validNumber = regExNumber.test(event.target.value);
+            if(!validNumber){
+                alertErrorSaldo.classList.remove('d-none');
+                msgErrorSaldo.innerText ='Solo se permiten numeros';
+                btnCalcular.disabled = true;  
+            }else{
+                saldoMensual = parseFloat(event.target.value);
+                
+                if( saldoMensual <= 0 ){
+                    alertErrorSaldo.classList.remove('d-none');
+                    msgErrorSaldo.innerText ='Debes ingresar un monto para calcular';
+                    btnCalcular.disabled = true;  
+                }else if(saldoMensual <= 100){
+                    alertErrorSaldo.classList.remove('d-none');
+                    msgErrorSaldo.innerText ='El monto mínimo debe ser mayor a 100 pesos';
+                    btnCalcular.disabled = true;  
+                }else{
+                    btnCalcular.disabled = false;
+                }
+            }
     }
+  
+
    
-})
+});
 
 document.querySelector('#pago').addEventListener('input',(event)=>{
-    alertErrorPago.classList.add('d-none')    
-    pago = parseFloat(event.target.value);
+    alertErrorPago.classList.add('d-none')   
+    
 
-    if( pago <= 0){
+    if( event.target.value.length < 1 || isNaN(event.target.value.length)){
         alertErrorPago.classList.remove('d-none')
         msgErrorPago.innerText='Debes ingresar un monto para calcular';
-        btnCalcular.disabled = true;        
+        btnCalcular.disabled = true; 
     }else{
-        btnCalcular.disabled = false;
-    }
+        
+        let validNumber = regExNumber.test(event.target.value);
+        if(!validNumber){
+            alertErrorPago.classList.remove('d-none')
+            msgErrorPago.innerText='Solo se permiten numeros';
+            btnCalcular.disabled = true;   
+        }else{
 
-   
-})
+            pago = parseFloat(event.target.value);
+            if( pago <= 0){
+                alertErrorPago.classList.remove('d-none')
+                msgErrorPago.innerText='Debes ingresar un monto para calcular';
+                btnCalcular.disabled = true;        
+            }else{
+                btnCalcular.disabled = false;
+            }
+        }
+
+    }
+});
 
 //Al submit checar que ambos inputs tengas valores mayor a cero
 btnCalcular.addEventListener('click',(event)=>{
@@ -91,9 +127,12 @@ btnCalcular.addEventListener('click',(event)=>{
         }else if( pago <= 0 || isNaN(pago)){
             alertErrorPago.classList.remove('d-none')
             msgErrorPago.innerText='Debes ingresar un monto para calcular';
+        }else if( pago > saldoMensual){
+            
+            alertErrorPago.classList.remove('d-none')
+            msgErrorPago.innerText='La cantidad debe ser menor al saldo mensual pendiente';
         }else{
-            componenteCaculadora.classList.add('d-none');
-            componenteResultados.classList.remove('d-none')
+            calcular_deuda(saldoMensual,pago);
         }
 
 })
@@ -102,3 +141,27 @@ btnNuevoCalculo.addEventListener('click',()=>{
             componenteCaculadora.classList.remove('d-none');
             componenteResultados.classList.add('d-none')
 })
+
+
+function calcular_deuda(saldo_mensual, pago){
+    
+    let resultado =0;
+    let deuda = saldo_mensual - pago;
+    let interes_generado = deuda * (0.0526);
+    let iva_intereses = interes_generado * 0.16;
+    resultado = deuda + interes_generado +  iva_intereses
+    
+    //Colocar datos de entrada en la vista
+    document.getElementById('entrada-saldo').innerText='$'+saldo_mensual.toFixed(2);
+    document.getElementById('entrada-pago').innerText='$'+pago.toFixed(2);
+
+    //Colocar datos de salida en la vista de la calculadora
+    document.querySelector('.info-cantidad').innerText ='$'+resultado.toFixed(2);
+    document.getElementById('deuda').innerText ='$'+deuda.toFixed(2);
+    document.getElementById('interes-deuda').innerText ='$'+interes_generado.toFixed(2);
+    document.getElementById('iva-interes').innerText ='$'+iva_intereses.toFixed(2);
+
+    componenteCaculadora.classList.add('d-none');
+    componenteResultados.classList.remove('d-none')
+   
+}
